fbffbd86b9f4332b07426b693cf17aa04a4e065e
m-nohira <csma17068@g.nihon-u.ac.jp>
2017-10-02 21:18:46 +0900

    プロジェクト ファイルを追加します。

---
+++ AstroneerAirLock/Light_Test.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VRageMath;
+using VRage.Game;
+using VRage.Library;
+using Sandbox.ModAPI.Interfaces;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.Common;
+using Sandbox.Game;
+using VRage.Collections;
+using VRage.Game.ModAPI.Ingame;
+using SpaceEngineers.Game.ModAPI.Ingame;
+
+namespace AstroneerAirLock
+{
+    class Light_Test: MyGridProgram
+    {
+        void Main(string argument)
+        {
+            var light1 = GridTerminalSystem.GetBlockWithName("AirLock1-CornerLight") as IMyLightingBlock;
+            var light2 = GridTerminalSystem.GetBlockWithName("AirLock1-CornerLigh-D") as IMyLightingBlock;
+
+            light1.Color = Color.Red;
+            light2.Color = Color.Red;
+        }
+    }
+}
